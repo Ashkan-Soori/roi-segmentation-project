1cf4e21a82d040461d7bcd16c1b843497057f7b7
Ashkan-Soori <153719730+Ashkan-Soori@users.noreply.github.com>
2026-06-15 01:56:25 +0200

    fix: remove TkAgg backend and stabilize overlay test; all tests passing

---
+++ tests/test_overlay (2).py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+from roi_segmentation.main import create_overlay
+
+
+def test_overlay_highlights_boundaries():
+    img = np.zeros((20,20,3), dtype=np.uint8)
+    mask = np.full((20,20), 255, dtype=np.uint8)
+
+    mask[5:15,5:15] = 0
+
+    overlay = create_overlay(img, mask)
+
+    assert not np.array_equal(overlay, img)
+
